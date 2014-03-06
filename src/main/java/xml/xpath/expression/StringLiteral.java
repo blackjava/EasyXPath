8b53f07cffc351269ecc80db0f9107c6b44fe032
Espen Gronli <espen@znork.net>
2014-03-06 10:05:18 +0100

    Added string literal.

---
+++ src/main/java/xml/xpath/expression/StringLiteral.java
@@ -0,0 +1,16 @@
+package xml.xpath.expression;
+
+import i18n.ErrorCode;
+import xml.xpath.Expression;
+
+public class StringLiteral implements Expression {
+    private static final String STRING_LITERAL_PATTERN = "(\\\"([^\\\"]|\\\\\\\"|\\\"\\\")*\\\"|\\'([^\\']|\\\\\\'|\\'\\')*\\')";
+    
+    public StringLiteral() {
+        throw new UnsupportedExpressionException(ErrorCode.UNSUPPORTED_EXPRESSION_STRING);
+    }
+    
+    public static boolean isStringLiteral(String expression) {
+        return expression.trim().matches(STRING_LITERAL_PATTERN);
+    }
+}
---
+++ src/test/java/xml/xpath/expression/StringLiteralTest.java
@@ -0,0 +1,45 @@
+package xml.xpath.expression;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringLiteralTest {
+    
+    @Test
+    public void quoted_string_is_recognized() {
+        assertTrue(StringLiteral.isStringLiteral("\"string 123\""));
+        assertTrue(StringLiteral.isStringLiteral("'string 123'"));
+    }
+    
+    @Test
+    public void string_with_unterminated_quote_is_not_recognized() {
+        assertFalse(StringLiteral.isStringLiteral("\"string 123"));
+        assertFalse(StringLiteral.isStringLiteral("'string 123"));
+    }
+
+    @Test
+    public void string_with_characters_outside_quotes_is_not_recognized() {
+        assertFalse(StringLiteral.isStringLiteral("\"string 123\" bad"));
+        assertFalse(StringLiteral.isStringLiteral("'string 123' bad"));
+    }
+
+    @Test
+    public void string_with_whitespace_outside_quotes_is_recognized() {
+        assertTrue(StringLiteral.isStringLiteral(" \"string 123\"\t\r\n"));
+        assertTrue(StringLiteral.isStringLiteral(" 'string 123'\t\r\n"));
+    }
+    
+    @Test
+    public void string_containing_unescaped_quote_is_not_recognized() {
+        assertFalse(StringLiteral.isStringLiteral("\"str\"ing\" 123"));
+        assertFalse(StringLiteral.isStringLiteral("'str'ing' 123"));
+    }
+    
+    @Test
+    public void string_containing_escaped_quote_is_recognized() {
+        assertTrue(StringLiteral.isStringLiteral("\"str\\\"ing 123\""));
+        assertTrue(StringLiteral.isStringLiteral("\"str\"\"ing 123\""));
+        assertTrue(StringLiteral.isStringLiteral("'str\\'ing 123'"));
+        assertTrue(StringLiteral.isStringLiteral("'str''ing 123'"));
+    }
+}
