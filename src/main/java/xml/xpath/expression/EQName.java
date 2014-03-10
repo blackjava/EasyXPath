d980a956b6b276e83acc50e6e557d745de55ace7
Espen Gronli <espen@znork.net>
2014-03-10 14:52:09 +0100

    Added EQName literal.

---
+++ src/main/java/xml/xpath/expression/EQName.java
@@ -0,0 +1,25 @@
+package xml.xpath.expression;
+
+import static xml.xpath.expression.QName.QNAME_PATTERN;
+import static xml.xpath.expression.URIQualifiedName.URIQUALIFIEDNAME_PATTERN;
+
+import i18n.ErrorCode;
+
+/**
+ * An expanded QName consists of an optional namespace URI and a local name. 
+ * An expanded QName also retains its original namespace prefix (if any), to 
+ * facilitate casting the expanded QName into a string. Two expanded QNames are 
+ * equal is their namespace URIs are equal and their local names are equal, even 
+ * if their namespace prefixes are not equal.
+ */
+public class EQName extends Literal {
+    private static String EQNAME_PATTERN = "(" + QNAME_PATTERN + "|" + URIQUALIFIEDNAME_PATTERN + ")";
+    
+    public EQName() {
+        throw new UnsupportedExpressionException(ErrorCode.UNSUPPORTED_LITERAL_EQNAME);
+    }
+    
+    public static boolean isEqName(String expression) {
+        return expression.matches(EQNAME_PATTERN);
+    }
+}
---
+++ src/test/java/xml/xpath/expression/EQNameTest.java
@@ -0,0 +1,31 @@
+package xml.xpath.expression;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EQNameTest {
+    @Test
+    public void name_without_prefix_is_recognized() {
+        assertTrue(EQName.isEqName("name"));
+    }
+
+    @Test
+    public void name_with_namespace_is_recognized() {
+        assertTrue(EQName.isEqName("namespace:name"));
+    }
+
+    @Test
+    public void name_with_two_namespaces_is_not_recognized() {
+        assertFalse(EQName.isEqName("namespace:namespace:name"));
+    }
+    
+    @Test
+    public void name_with_braced_uri_identifier_is_recognized() {
+        assertTrue(EQName.isEqName("Q{http://test.com}name"));
+    }
+
+    @Test
+    public void name_with_braced_uri_identifier_and_namespace_is_not_recognized() {
+        assertFalse(EQName.isEqName("Q{http://test.com}namespace:name"));
+    }
+}
