2f48e54485c083e55feed1e21514d8efa30f25e2
Espen Gronli <espen@znork.net>
2014-03-19 12:33:45 +0100

    Added instantiation of decimal literal.

--- src/main/java/xml/xpath/expression/DecimalLiteral.java
+++ src/main/java/xml/xpath/expression/DecimalLiteral.java
@@ -9,8 +9,29 @@ import i18n.ErrorCode;
 public class DecimalLiteral extends Literal {
     protected static final String DECIMAL_LITERAL_PATTERN = "(\\.[0-9]+|[0-9]+\\.[0-9]*)";
     
-    public DecimalLiteral() {
-        throw new UnsupportedExpressionException(ErrorCode.UNSUPPORTED_LITERAL_DECIMAL);
+    private final Float value;
+    
+    /**
+     * Construct a decimal literal from a string.
+     * @throws NumberFormatException if the string is not a valid decimal
+     */
+    public DecimalLiteral(String value) {
+        try {
+            this.value = Float.parseFloat(value);
+        } catch (java.lang.NumberFormatException nfe) {
+            for (int index = 0; index < value.length(); index++) {
+                if (!Character.isDigit(value.charAt(index))) {
+                    throw new NumberFormatException(nfe, ErrorCode.NUMBER_CONTAINS_INVALID_CHARACTER, value, Integer.toString(index));
+                }
+            }
+
+            throw new NumberFormatException(nfe, ErrorCode.COULD_NOT_PARSE_DECIMAL, value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Float.toString(value);
     }
     
     public static boolean isDecimalLiteral(String expression) {
